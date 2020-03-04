7bf337d13260fe588a7d152381089a3bf18b6634
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-03-04 14:21:41 -0600

    Initial commit

---
+++ forms/animals/animals.js
@@ -0,0 +1,11 @@
+//create array named myAnimals
+let myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+
+//gets name of an animal from user and adds it to end of array
+let userAnimals = prompt("Enter an animal to add it to the array: ")
+      userAnimals = userAnimal.toLowerCase()
+myAnimals.push(userAnimal)
+
+//outputs the last animal in the array to an alert
+let lastAnimal = myAnimals.slice(-1)[0])
+alert(`The last animal is a/an ${lastAnimal}.`)
